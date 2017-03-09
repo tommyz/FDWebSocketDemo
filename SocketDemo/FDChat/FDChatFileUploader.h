67b9953d4d23553a15105b9cf4a0e90c2ff2b4e0
xietao <xietao3@qq.com>
2017-03-09 15:20:52 +0800

    增加上传图片功能

--- SocketDemo/FDChat/FDChatFileUploader.h
+++ SocketDemo/FDChat/FDChatFileUploader.h
@@ -8,6 +8,13 @@
 
 #import <Foundation/Foundation.h>
 
+/* 支持上传的文件类型 (可增加) */
+typedef NS_ENUM(NSUInteger, FDChatFileType) {
+    FDChatFileTypeImage = 0,    // 图片(jpg)
+    FDChatFileTypeVoice,        // 音频(mp3)
+    FDChatFileTypeVideo         // 视频(mov)
+};
+
 @interface FDChatFileUploader : NSObject
 
 @end
--- SocketDemo/FDChat/FDChatFileUploader.m
+++ SocketDemo/FDChat/FDChatFileUploader.m
@@ -7,7 +7,100 @@
 //
 
 #import "FDChatFileUploader.h"
+#import <AFNetworking/AFNetworking.h>
+
+// 图片压缩比例  0 ~ 1.0
+static CGFloat const imageCompressionQuality = 0.5;
+static NSString *const uploadFileUrl = @"www.baidu.com";
 
 @implementation FDChatFileUploader
+
+
++ (void)uploadImage:(UIImage *)image
+           progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+            success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+            failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure {
+    [self uploadFileData:UIImageJPEGRepresentation(image,imageCompressionQuality)
+                    name:@"image"
+                fileName:@"imageName"
+                fileType:FDChatFileTypeImage
+                progress:uploadProgress
+                 success:success failure:failure];
+}
+
++ (void)uploadFileData:(NSData *)fileData
+                  name:(NSString *)name
+              fileName:(NSString *)fileName
+              fileType:(FDChatFileType)fileType
+              progress:(nullable void (^)(NSProgress * _Nonnull))uploadProgress
+               success:(void (^)(NSURLSessionDataTask *task, id responseObject))success
+               failure:(void (^)(NSURLSessionDataTask *task, NSError *error))failure {
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json", @"text/html", nil];
+    // 检测网络
+    if (![self beforeRequest:manager]) {
+        if (failure) {
+            failure(nil,nil);
+        }
+    }
+    
+    // 参数
+//    NSMutableDictionary *completeParams = [NSMutableDictionary dictionaryWithDictionary:@{}];
+    [manager POST:uploadFileUrl parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        
+        if (fileType == FDChatFileTypeImage) {
+            // jpg图片格式
+            [formData appendPartWithFileData:fileData name:name  fileName:fileName mimeType:@"image/jpeg"];
+        }else if (fileType == FDChatFileTypeVoice) {
+            // mp3音频格式
+            [formData appendPartWithFileData:fileData name:name  fileName:fileName mimeType:@"audio/mpeg"];
+        }else if (fileType == FDChatFileTypeVideo) {
+            // mov视频格式
+            [formData appendPartWithFileData:fileData name:name  fileName:fileName mimeType:@"video/quicktime"];
+        }
+        
+    } progress:^(NSProgress * _Nonnull progress) {
+        if (uploadProgress) {
+            uploadProgress(progress);
+        }
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        if (success) {
+            success(task,responseObject);
+        }
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        if (failure) {
+            failure(task,error);
+        }
+    }];
+}
+
+
+/**
+ *  请求前检查网络情况
+ *
+ *  @param manager AFHTTPRequestOperationManager
+ *
+ *  @return 网络正常则返回YES，否则返回NO
+ */
++ (BOOL)beforeRequest:(AFHTTPSessionManager *)manager {
+    NSOperationQueue *operationQueue = manager.operationQueue;
+    [manager.reachabilityManager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                [operationQueue setSuspended:NO];
+                break;
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                [operationQueue setSuspended:NO];
+                break;
+            case AFNetworkReachabilityStatusNotReachable:
+            default:
+                [operationQueue setSuspended:YES];
+                break;
+        }
+    }];
+    [manager.reachabilityManager startMonitoring];
+    return true;
+}
 
 @end
