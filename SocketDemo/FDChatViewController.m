b66007abca092c542ebd895526a3fa4571719f10
xieyan <xieyan@xieyandeMacBook-Pro.local>
2017-03-15 13:51:48 +0800

    去除延时

--- SocketDemo/FDChatViewController.m
+++ SocketDemo/FDChatViewController.m
@@ -98,9 +98,7 @@
     //加载数据
     self.currentPage = 0;
     [self loadMoreMessages];
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [self  chatTableViewScrollToBottom];
-    });
+    [self  chatTableViewScrollToBottom];
     
     if (![FDWebSocket socketIsConnected]) {
         [self openSocket];
